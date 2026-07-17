6256705da1fbbc7ea3a864b0b131b44c24678e0d
Gabriel Costa Fassarella <gabrielcfassarella91@gmail.com>
2026-07-17 15:03:22 -0300

    first commit

---
+++ filafacil/src/main/java/fifafacil/example/filafacil/FilafacilApplication.java
@@ -0,0 +1,13 @@
+package fifafacil.example.filafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilafacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilafacilApplication.class, args);
+	}
+
+}
---
+++ filafacil/src/test/java/fifafacil/example/filafacil/FilafacilApplicationTests.java
@@ -0,0 +1,13 @@
+package fifafacil.example.filafacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilafacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
